df9461608c0cad3997bff317fca8aa8d94e85f47
刘文 <linus_max@163.com>
2024-01-29 10:34:26 +0800

    fix

---
+++ TZImagePickerController/TZImagePickerController/TZMedia.h
@@ -0,0 +1,17 @@
+//
+//  TZMedia.h
+//  TZImagePickerController
+//
+//  Created by Mark on 2024/1/29.
+//  Copyright © 2024 谭真. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TZMedia : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TZImagePickerController/TZImagePickerController/TZMedia.m
@@ -0,0 +1,13 @@
+//
+//  TZMedia.m
+//  TZImagePickerController
+//
+//  Created by Mark on 2024/1/29.
+//  Copyright © 2024 谭真. All rights reserved.
+//
+
+#import "TZMedia.h"
+
+@implementation TZMedia
+
+@end
